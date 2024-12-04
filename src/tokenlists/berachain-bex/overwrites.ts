8cb15f59326b6983b4741daecb99b827ae49699c
hunter-bera <133678627+hunter-bera@users.noreply.github.com>
2024-12-04 16:32:51 -0500

    add pyth price feed

--- src/tokenlists/berachain-bex/overwrites.ts
+++ src/tokenlists/berachain-bex/overwrites.ts
@@ -5,16 +5,22 @@ export const overwrites: OverwritesForList = {
     '0x015fd589F4f1A33ce4487E12714e1B15129c9329': {
       extensions: {
         coingeckoId: 'usd-coin',
+        pythPriceId:
+          '0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a', // USDC/USD
       },
     },
     '0x164A2dE1bc5dc56F329909F7c97Bae929CaE557B': {
       extensions: {
         coingeckoId: 'tether',
+        pythPriceId:
+          '0x2b89b9dc8fdf9f34709a5b106b472f0f39bb6ca9ce04b0fd7f2e971688e2e53b', // USDT/USD
       },
     },
     '0x2d93FbcE4CffC15DD385A80B3f4CC1D4E76C38b3': {
       extensions: {
         coingeckoId: 'weth',
+        pythPriceId:
+          '0x9d4294bbcd1174d6f2003ec365831e64cc31d9f6f15a2b85399db8d5000960f6', // WETH/USD
       },
     },
   },
